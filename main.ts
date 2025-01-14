ae4f212b7fd62b3b973270dfe6edbd5460ced258
Daria <qqdashkan@icloud.com>
2025-01-14 18:00:47 +0200

    changed printPersonInfo function

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ function printPersonInfo(person: any): asserts person is User {
   if ('name' in person && 'age' in person) {
     const { name, age } = person;
     console.log(`Name: ${name}, age: ${age}`);
-  } else throw new Error('Not an user');
+  } else throw new Error('Not a person');
 }
 
 printPersonInfo(user);
