ef4f0b0cb08cc174a3ebbc0c9cc1da2ff40bb48d
Daria <qqdashkan@icloud.com>
2025-01-14 17:59:03 +0200

    changed printPersonInfo function

--- main.ts
+++ main.ts
@@ -13,16 +13,10 @@ function fetchData(): unknown {
 const user: User = fetchData() as User;
 
 function printPersonInfo(person: any): asserts person is User {
-  if (person === null && typeof person !== 'object') {
-    throw new Error(`${person} must be of type object`);
-  } else if (!('name' in person) && typeof person.name !== 'string') {
-    throw new Error(`${person.name} must be of type string`);
-  } else if (!('age' in person) && typeof person.age !== 'number') {
-    throw new Error(`${person.age} must be of type number`);
-  }
-
-  const { name, age } = person;
-  console.log(`Name: ${name}, age: ${age}`);
+  if ('name' in person && 'age' in person) {
+    const { name, age } = person;
+    console.log(`Name: ${name}, age: ${age}`);
+  } else throw new Error('Not an user');
 }
 
 printPersonInfo(user);
